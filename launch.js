b8e36fc590530c49d41c392f670f42359c02ab1a
unknown <ansarimehek10@gmail.com>
2024-06-13 17:46:03 +0530

    practice

--- launch.js
+++ launch.js
@@ -1,3 +1,229 @@
-console.log (hello )
-console.log (hey )
+// console.log ("hello ")
+// console.log ("hey" )
 
+// variables 
+//  let messages="the password is incorrect";
+//  define the variables and assign value it 
+// console.log (messages )
+//alert (messages )
+
+// define variable hello and assign as hii 
+// let hello= hii;
+// console.log (hello );
+
+// var newdata="mehek";
+// console.log (newdata ) 
+
+// let messagebox;
+//  messagebox="world ";
+//  messagebox="hello ";
+// console.log (messagebox );
+
+// declare two variables:admin and name.
+// assign the value "john" to name.
+// copy the value of admin using alert (must output "john")
+// let admin;
+// name="anas"
+// admin=name;
+// alert(admin);
+
+// constants
+// const birthday="08.12.2001";
+// birthday="10.11.2001";
+// console.log (birthday);
+
+//  refer URL
+//  https://javascript.info/variables
+
+// Data types in JS
+// 1.number
+// let n=123;
+// let nf 12.344;
+
+// 2.bigint
+//  let bigint=1234567890n;
+
+// 3.string
+// let str="hello"
+// let str2="single quotes are ok too";
+//  let phrase=`${str} can embed another ${str2}`;
+
+// 4.boolean
+//  let greater = 4 < 1;
+//  alert(greater)
+
+// 5.null
+//  it's just a special value which represents "nothing", "empty" or "value unknown."
+// let age=null;
+//  console.log(age)
+ 
+
+// 6 .undefine
+//  let data;
+// console.log(data)
+
+// 7. object for more complex data structures.
+
+
+// 8. symbol for unique identifiers.age
+
+// URL
+// https://javascript.info/types
+
+// basic opertors 
+
+// addition 
+// let apples="2"
+// let oranges="3"
+// alert(apples + oranges)
+
+// 1.arithmetic operator
+// a1 = 20; 
+// a2 = 30;
+// a3 = a1 + a2;
+// console.log(a3)
+
+// 2.assignment operator
+// a1 = 20; //a1 = 20+
+// a1 = 30;
+
+// let n=2;
+// n+=5; //now n= 7 (same as n = n + 5)
+// n*=2; //now n=14 (same as n = n * 2)
+
+// alert (n); //14
+
+//  let x = 1;
+//  x = -x;
+//  alert( x ); //unary negation was applied 
+
+// let z=1, y=3;
+// alert (y-z);
+
+// division 8/3
+// alert (8 % 3)
+
+// exponentional 2²      
+// alert( 2 ** 2 ); // 2² = 4  
+// alert( 2 ** 3 ); // 2³ = 8  
+// alert( 2 ** 4 ); // 2⁴ = 16    
+
+// '1' is string ... therefore value is 12  
+// alert( '1' + 2 ); // "12" 
+// alert( 2 + '1' ); // "21"
+
+// alert(2 + 2 + '1' );     
+
+// 3.string operator   
+// let s = "my" + "string";  
+// alert(s); // mystring   
+
+// 4.increment and decement operator   
+// ++x pre-increment ... increment x by 1 then return x   
+// x++ post-increment ... return x than increment by 1  
+// --x pre-decrement ... decrement x by 1 then return x  
+// x-- post-decrement ... return x than decrement by 1
+
+//  let f;  
+//  f = 10;  
+//  console.log(++f); //11  
+//  console.log(f); //11   
+
+//  let g;  
+// g = 12;  
+//  console.log(g++); //12  
+//  console.log(g); //13    
+
+//  let h;  
+//  h = 10;  
+//  console.log(--h); //9  
+//  console.log(h); //9   
+
+//  let i;  
+//  i = 12;  
+//  console.log(i--); //12  
+//  console.log(i); //11  
+
+ // 5.logical operator  
+ // && Logical AND: true if both the operands/boolean values are true, else evaluates to false  
+ // || Logical OR: true if either of the operands/boolean values is true. evaluates to false if both are false  
+ // ! Logical NOT: true if the operand is false and vice-versa.      
+ 
+//   AND &&  
+//  const a = true, b = false; 
+//   const c = 4; 
+//  console.log(a && a); // true  
+//  console.log(a && b); // false  
+//  console.log((c > 2) && (c < 2)); // false   
+
+// || OR   
+//  const a = true, b = false, c = 4;  
+//  console.log(a || b); // true  
+//  console.log(b || b); // false  
+//  console.log((c>2) || (c<2)); // true 
+
+ // ! NOT   
+ // const a = true, b = false;  
+ // console.log(!a); // false  
+ // console.log(!b); // true    
+ 
+ // url   
+ // https://javascript.info/operators    
+ 
+ // 6. comparison operator   
+//   x=5  
+//   x =="5" //true  
+//   x === "5" //false   
+ 
+ // url   
+ // https://www.w3schools.com/js/js_comparisons.asp    
+ 
+ // 7.condition operator  
+//   let company = prompt('which company created javascript?');  
+//   if(company == 'nestscape') //{  
+// console.log("you are right");  // }else{  
+//  console.log("you are wrong");  // }    
+
+// company == 'nestscape' ? 
+//  consol/e.log("you are right") : 
+//  console.log("you are wrong");    
+
+// functions   
+// url   
+// https://javascript.info/function-basics      
+
+// Array   
+// const fruites = ["Bannana", "Mango", "Orange", "Apple"];  
+// index..... 0 1 2 3  
+// length 4    
+
+// Properties of Array 
+
+// join()  
+// The join() method also joins all array elements into a string.
+
+// pop()  
+// The pop() method removes the last element from an array:
+
+// push()  
+// The push() method adds a new element to an array (at the end): 
+
+// shift()  
+// The shift() method removes the first array element and "shifts" all other elements to a lower index.
+
+// unshift()  
+// The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements: 
+
+// splice()  
+// The splice() method can be used to add new items to an array:  
+
+// concat() 
+ // The concat() method creates a new array by merging (concatenating) existing arrays:  
+
+// slice() 
+// The slice() method slices out a piece of an array into a new array.    
+
+// sort()  
+// reverse() 
+
+ // URL https://www.w3schools.com/js/js_array_methods.asp
